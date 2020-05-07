01b190edceb1f14854e2142ddfa1c03753c89702
Oliver Mayer <mayer@adbk-nuernberg.de>
2020-05-08 00:54:46 +0200

    VS2017 and MacOS builds added, logging component added, midi settings added

--- JUCE GUI/Moving-Speakers-GUI/Source/MainComponent.cpp
+++ JUCE GUI/Moving-Speakers-GUI/Source/MainComponent.cpp
@@ -12,10 +12,26 @@
 MainComponent::MainComponent()
 {
     setSize (600, 400);
+
+	Logger::setCurrentLogger(&_logComponent);
+
+	File midiSettingsFile(File::getCurrentWorkingDirectory().getChildFile("../../midiconfig.json"));
+
+	if(_midiSettings.load(midiSettingsFile)) {
+
+		Logger::writeToLog(_midiSettings.toString());
+	}
+	else {
+
+		AlertWindow::showMessageBox(AlertWindow::WarningIcon, "Error", "Failed to load MIDI settings from file 'midiconfig.json'", "Quit", this);
+	
+		JUCEApplication::getInstance()->systemRequestedQuit();
+	}
 }
 
 MainComponent::~MainComponent()
 {
+	Logger::setCurrentLogger(nullptr);
 }
 
 //==============================================================================
@@ -24,9 +40,13 @@ void MainComponent::paint (Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+    //g.setFont (Font (16.0f));
+    //g.setColour (Colours::white);
+    //g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+    
+    
+	addAndMakeVisible(_logComponent);
+	addAndMakeVisible(_tabsComponent);
 }
 
 void MainComponent::resized()
@@ -34,4 +54,16 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    
+    FlexBox fb;
+	fb.flexDirection = FlexBox::Direction::column;
+	fb.flexWrap = FlexBox::Wrap::noWrap;
+	fb.justifyContent = FlexBox::JustifyContent::center;
+	fb.alignContent = FlexBox::AlignContent::stretch;
+	fb.alignItems = FlexBox::AlignItems::stretch;
+
+	fb.items.add(FlexItem(600, 300, _tabsComponent).withFlex(0.9f, 1.1f, 1.0f));
+	fb.items.add(FlexItem(600, 100, _logComponent));
+	
+	fb.performLayout(getLocalBounds().toFloat());
 }
